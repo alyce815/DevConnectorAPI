f153a1a609af81598ac610ff142626fe795f0a5a
Alyce <alyce815@yahoo.com>
2019-03-28 16:05:36 -0700

    this is the first ten lines of code for the server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import Issue from '/models/Issue';
+
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://[server]/issues');
